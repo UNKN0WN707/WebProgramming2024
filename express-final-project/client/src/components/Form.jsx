c6c2a9fe83867a58d6986f9b8a627c2f183be9f4
Andy Tran <andytranbusiness123@gmail.com>
2024-03-26 12:32:28 -0400

    Finished form

--- express-final-project/client/src/components/Form.jsx
+++ express-final-project/client/src/components/Form.jsx
@@ -10,24 +10,28 @@
 import React from 'react';
 import './Form.css';
 
-const Form = () => {
+const Form = ({ changeValue }) => {
+
+
     return (
         <div>
             <div className='foreground'>
                 <div className='howTo'>
                     <h2>How To Use</h2>
-                    <p>To use the Anime Collection Tracker, please enter the name of the anime in the 
-                        Anime Entry Form. You can then rate the anime you watched from a scale of 1 to
-                        10 using the slider. Hit submit and the anime you enter will get stored in our
-                        database.
+                    <p>There are a lot of Anime out there and we are here to help you
+                        keep track of all of them! To use the Anime Collection Tracker, please enter 
+                        the name of the anime in the Anime Entry Form. You can then rate the anime you 
+                        watched from a scale of 1 to 10 using the slider. Hit submit and the anime you 
+                        entered will get stored in our database.
                     </p>
                 </div>
                 
                 <form>
                     <p>Anime Entry Form</p><br/>
                     <input type="text" id="anime" placeholder="Enter Anime Here" /><br/><br/>
-                    <input type="range" min="1" max="100" /><br/><br/>
-                    <input type="submit" value="Submit" />
+                    <p id="score">Score: 5</p>
+                    <input type="range" min="1" max="10" defaultValue="5" onChange={changeValue} id="slider" /><br/><br/>
+                    <input type="submit" value="Submit" className="button" />
                 </form>
             </div>
         </div>
--- express-final-project/client/src/components/Home.jsx
+++ express-final-project/client/src/components/Home.jsx
@@ -13,11 +13,21 @@ import Hero from './Hero';
 import Form from './Form';
 
 const Home = () => {
+    
+
+    const changeValue = () => {
+        const slider = document.getElementById("slider");
+        const value = document.getElementById("score");
+        const results = "Score: " + slider.value;
+
+        value.textContent = results
+    }     
+
     return(
         <div>
             <Navbar />
             <Hero />
-            <Form />
+            <Form changeValue={changeValue}/>
         </div>  
     );
 }
